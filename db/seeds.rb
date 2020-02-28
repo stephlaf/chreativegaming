decb4a0365407bc31dd9571678465f913cd88a53
Stephane Lafontaine <stephane@medqigong.ca>
2020-02-28 12:58:05 -0500

    Added urls in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,22 @@ Game.destroy_all if Rails.env.development?
 
 puts "Seeding games..."
 
+urls = [
+  'https://www.designzzz.com/wp-content/uploads/2013/04/Angry-bird-wallpaper-iphone.jpg',
+  'https://www.designzzz.com/wp-content/uploads/2013/04/Assassins-Creed-wallpaper-iphone.jpg',
+  'https://www.designzzz.com/wp-content/uploads/2013/04/Battlefield-Aircraft-wallpaper-iphone.jpg',
+  'https://www.designzzz.com/wp-content/uploads/2013/04/Crysis_3_game_wallpaper.jpg',
+  'https://www.designzzz.com/wp-content/uploads/2013/04/Devil-May-Cry-game-3.jpg',
+  'https://www.designzzz.com/wp-content/uploads/2013/04/Devil-May-Cry-game-wallpaper.jpg',
+  'https://www.designzzz.com/wp-content/uploads/2013/04/Diablo-lineage-wallpaper.jpg',
+  'https://www.designzzz.com/wp-content/uploads/2013/04/Fantasy-game-Diablo-wallpaper.jpg',
+  'https://www.designzzz.com/wp-content/uploads/2013/04/Online-games-DNF.jpg',
+  'https://www.designzzz.com/wp-content/uploads/2013/04/Angry-birds-rio-wallpaper.jpg'
+]
+
+i = 0
 10.times do
-  file = URI.open('https://giantbomb1.cbsistatic.com/uploads/original/9/99864/2419866-nes_console_set.png')
+  file = URI.open(urls[i])
   g = Game.new(
     name: Faker::Games::HeroesOfTheStorm.unique.battleground,
     description: Faker::Games::LeagueOfLegends.quote,
@@ -19,6 +33,7 @@ puts "Seeding games..."
     )
   g.thumbnail.attach(io: file, filename: 'rand.jpg', content_type: 'image/jpg')
   g.save!
+  i += 1
 end
 
 puts "All done"
