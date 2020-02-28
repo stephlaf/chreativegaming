e0d95a4f195f847e9c51dacdb5a9d2246cd3dc63
Stephane Lafontaine <stephane@medqigong.ca>
2020-02-28 13:56:18 -0500

    Added community & about pages & corresponding scss files; added temp divs in home page & rudimental scss

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  # root to: 'pages#home'
-  root to: 'games#index'
+  root to: 'pages#home'
+
+  get 'about', to: 'pages#about'
+  get 'community', to: 'pages#community'
 
   resources :games
   
