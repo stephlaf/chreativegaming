2e445d476916bfec9dd4ca75b7d4a78bc6694bd7
Stephane Lafontaine <stephane@medqigong.ca>
2020-03-05 13:30:44 -0500

    Changed username to gametag in users; added link in navbar avatar to get to user show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_02_184023) do
+ActiveRecord::Schema.define(version: 2020_03_05_181211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,7 +77,7 @@ ActiveRecord::Schema.define(version: 2020_03_02_184023) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "username"
+    t.string "gametag", null: false
     t.string "first_name"
     t.string "last_name"
     t.string "status"
