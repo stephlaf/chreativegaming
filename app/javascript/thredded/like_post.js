9a34998af0349d6a069ec157c5819effc144fa31
Stephane Lafontaine <stephane@medqigong.ca>
2020-06-10 18:53:36 -0400

    Structured JS code as per best practice; added logic for syntax display of Like vs Likes

---
+++ app/javascript/thredded/like_post.js
@@ -0,0 +1,40 @@
+const likesLinks = document.querySelectorAll('.likes-link');
+const token = $('meta[name="csrf-token"]').attr('content');
+
+const updateCount = (data) => {
+  if (data.notice) alert(data.notice);
+  const likesCount = document.querySelector(`[data-count='count-${data.post.id}']`);
+  likesCount.innerText = `${data.count} Like${data.count === 1 ? '' : 's'}`;
+};
+
+const updateLikes = (postId) => {
+  fetch(`/${postId}/likes`, {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    method: 'POST',
+    headers: {
+      'X-Requested-With': 'XMLHttpRequest',
+      'X-CSRF-Token': token
+    },
+    body: 'test',
+    credentials: 'same-origin'
+  })
+    .then(response => response.json())
+    .then(data => updateCount(data));
+};
+
+const extractPostId = (event) => {
+  event.preventDefault();
+  const dataId = event.currentTarget.dataset.id;
+  const postIdStr = dataId.match(/^(post-)(\d+)$/)[2];
+  const postId = Number.parseInt(postIdStr, 10);
+  updateLikes(postId);
+};
+
+const likePost = () => {
+  likesLinks.forEach((likeLink) => {
+    likeLink.addEventListener('click', extractPostId);
+  });
+};
+
+export { likePost };
