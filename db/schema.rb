43d65a53aecd335931897458c9a485faf052b2c5
Stephane Lafontaine <stephane@medqigong.ca>
2021-01-03 17:41:45 -0500

    Migration fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_02_213642) do
+ActiveRecord::Schema.define(version: 2021_01_02_233541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
