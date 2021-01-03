e1af71ada6744d1b1c60cc0e7208c928e2962053
Stephane Lafontaine <stephane@medqigong.ca>
2021-01-03 14:16:49 -0500

    Completed same logic for forum post as for blog post

--- app/controllers/concerns/thredded/new_post_params.rb
+++ app/controllers/concerns/thredded/new_post_params.rb
@@ -6,23 +6,24 @@ module Thredded
     protected
 
     def new_post_params
-      # raise
       params.fetch(:post, {})
         .permit(:content, :quote_post_id, :priority_post, :status)
         .tap do |p|
-        p[:priority_post] = p[:priority_post] == '1' ? true : false
-        case p[:status]
-        when 'Regular'   then p[:status] = 0
-        when 'Priority'  then p[:status] = 1
-        when 'Published' then p[:status] = 2
+          # p[:priority_post] = p[:priority_post] == '1' ? true : false
+          unless p[:status].nil?
+            case p[:status].downcase
+            when 'regular'   then p[:status] = 0
+            when 'priority'  then p[:status] = 1
+            when 'published' then p[:status] = 2
+            end
+          end
+          quote_id = p.delete(:quote_post_id)
+          if quote_id
+            post = Thredded::Post.find(quote_id)
+            authorize_reading post
+            p[:quote_post] = post
+          end
         end
-        quote_id = p.delete(:quote_post_id)
-        if quote_id
-          post = Thredded::Post.find(quote_id)
-          authorize_reading post
-          p[:quote_post] = post
-        end
-      end
     end
   end
 end
