c724b87ab6661c6f0803f3334bbc48a3105fb661
Stephane Lafontaine <stephane@medqigong.ca>
2021-01-31 16:47:15 -0500

    Hand made pagination; might want to replace this with Pagy gem eventually, TBD

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ require 'open-uri'
 
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!
+
+  POSTS_PER_PAGE = 10
   
   def home
     file = URI.open('https://res.cloudinary.com/chreative-gaming/image/upload/v1583507597/default_junymf.png')
@@ -13,7 +15,21 @@ class PagesController < ApplicationController
     end
     @priority_posts = priorities
     @published_posts = published
-    @regular_posts = regular_posts
+    all_posts = regular_posts
+
+    if (all_posts.count % POSTS_PER_PAGE).zero?
+      @last_page = (all_posts.count / POSTS_PER_PAGE) - 1
+    else
+      @last_page = all_posts.count.fdiv(POSTS_PER_PAGE).floor
+    end
+
+    @page = params.fetch(:page, 0).to_i
+
+    lower_limit = @page * POSTS_PER_PAGE
+    upper_limit = (@page * POSTS_PER_PAGE) + (POSTS_PER_PAGE - 1)
+
+    @regular_posts = all_posts[lower_limit..upper_limit]
+    # raise
   end
 
   def about
