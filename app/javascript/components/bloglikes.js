bcf93aa7d31438cc7ff92f5fbd47e39ffc3f3c58
Alina Gotcherian <alina.gotcherian@gmail.com>
2021-05-05 09:38:58 -0400

    trying to make blog likes dynamic with JS

--- app/javascript/components/bloglikes.js
+++ app/javascript/components/bloglikes.js
@@ -9,4 +9,37 @@ const askForLogin = () => {
   }
 };
 
-export { askForLogin };
+const blogLike = () => {
+  const hearts = document.querySelectorAll('.blog-like');
+  const blogCounts = document.querySelectorAll('.blog-count');
+
+  console.log(blogCounts);
+  hearts.forEach((heart) => {
+    heart.addEventListener("click", () => {
+
+
+
+      let indexSearched;
+      blogCounts.forEach((count, index) => {
+        if (count.dataset.id === heart.dataset.id) {
+          indexSearched = index;
+        }
+      });
+
+
+      console.log("what we have to increment or decrement " ,blogCounts[indexSearched])
+      fetch(`/blog_posts/${heart.dataset.id}/blog_likes`)
+        .then(response => {
+          if (response.status === 200) {
+          // if heart is empty (has fas class) then add 1
+            blogCounts[indexSearched].innerText = parseInt(blogCounts[indexSearched].innerText, 10) + 1
+
+          // otherwise (has far class) remove 1
+            blogCounts[indexSearched].innerText = parseInt(blogCounts[indexSearched].innerText, 10) - 1
+          }
+        })
+    })
+  })
+};
+
+export { askForLogin, blogLike };
--- app/javascript/components/index.js
+++ app/javascript/components/index.js
@@ -1,10 +1,11 @@
-import { askForLogin } from './bloglikes';
+import { askForLogin, blogLike } from './bloglikes';
 import { displayFile } from './customUpload';
 import { showComingSoon } from './alerts';
 import { showTabContent } from './tabs';
 
 document.addEventListener('turbolinks:load', () => {
   askForLogin();
+  blogLike();
   displayFile();
   showComingSoon();
   showTabContent();
