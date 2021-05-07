1bdd3f8749332a8b32a64e6bd08b7b2b43c1bdd5
Alina Gotcherian <alina.gotcherian@gmail.com>
2021-05-07 09:53:50 -0400

    fixes and bloglikes js

--- app/javascript/components/bloglikes.js
+++ app/javascript/components/bloglikes.js
@@ -13,33 +13,31 @@ const blogLike = () => {
   const hearts = document.querySelectorAll('.blog-like');
   const blogCounts = document.querySelectorAll('.blog-count');
 
-  console.log(blogCounts);
-  hearts.forEach((heart) => {
-    heart.addEventListener("click", () => {
+  if (hearts) {
+    hearts.forEach((heart, index) => {
+      heart.addEventListener("click", (e) => {
+        e.preventDefault()
 
-
-
-      let indexSearched;
-      blogCounts.forEach((count, index) => {
-        if (count.dataset.id === heart.dataset.id) {
-          indexSearched = index;
-        }
-      });
-
-
-      console.log("what we have to increment or decrement " ,blogCounts[indexSearched])
-      fetch(`/blog_posts/${heart.dataset.id}/blog_likes`)
-        .then(response => {
-          if (response.status === 200) {
-          // if heart is empty (has fas class) then add 1
-            blogCounts[indexSearched].innerText = parseInt(blogCounts[indexSearched].innerText, 10) + 1
-
-          // otherwise (has far class) remove 1
-            blogCounts[indexSearched].innerText = parseInt(blogCounts[indexSearched].innerText, 10) - 1
-          }
-        })
+        fetch(`/blog_posts/${heart.dataset.id}/blog_likes`)
+          .then(response => {
+            if (response.status === 200) {
+              const oldCountLike = parseInt(blogCounts[index].innerText, 10)
+              if (heart.classList.contains("far")) {
+                // console.log("not 🧡, make it liked...")
+                blogCounts[index].innerText = oldCountLike + 1
+                heart.classList.replace("far", "fas");
+                // console.log("...Done");
+              } else {
+                // console.log("🧡, make it disliked...")
+                blogCounts[index].innerText = oldCountLike - 1
+                heart.classList.replace("fas", "far");
+                // console.log("...Done");
+              }
+            }
+          })
+      })
     })
-  })
+  };
 };
 
 export { askForLogin, blogLike };
