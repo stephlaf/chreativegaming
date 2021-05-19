5dfcdb1d582bb7dac428525adfb0a692d138dfe0
Stephane Lafontaine <stephane@medqigong.ca>
2021-05-19 13:12:24 -0400

    Moved protect_from_forgery with: :exception, prepend: true to ApplContr at top of class

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception, prepend: true
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  protect_from_forgery with: :exception, prepend: true
 
   include Pundit
 
