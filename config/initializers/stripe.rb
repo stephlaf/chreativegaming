35fd61b4887f1ab09573d22ceac0cf3c836af7ea
Stephane Lafontaine <stephane@medqigong.ca>
2023-05-29 17:19:29 -0400

    Added secret Stripe keys & logic to differentiate development (local) & production environments WIP

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,15 +1,24 @@
 require_relative 'stripe_checkout_session_service'
 
-Rails.configuration.stripe = {
-  publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
-  secret_key:      ENV['STRIPE_SECRET_KEY'],
-  signing_secret:  ENV['STRIPE_WEBHOOK_SECRET_KEY']
-}
+if Rails.env.development?
+  Rails.configuration.stripe = {
+    publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
+    secret_key:      ENV['STRIPE_SECRET_KEY'],
+    signing_secret:  ENV['STRIPE_WEBHOOK_SECRET_KEY_LOCAL']
+  }
+else
+  Rails.configuration.stripe = {
+    publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
+    secret_key:      ENV['STRIPE_SECRET_KEY'],
+    signing_secret:  ENV['STRIPE_WEBHOOK_SECRET_KEY_DEVELOPMENT']
+  }
+end
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
 StripeEvent.signing_secret = Rails.configuration.stripe[:signing_secret]
 
 StripeEvent.configure do |events|
+  pp events
   events.subscribe 'checkout.session.completed' do |event|
     StripeCheckoutSessionService.new.call(event)
   end
