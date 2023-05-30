0f988b9eecca5851751bb6158c088f9117164acf
Stephane Lafontaine <stephane@medqigong.ca>
2023-05-29 21:54:14 -0400

    Cleaned up test lines & added secret key for production webhook

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,6 @@
 require_relative 'stripe_checkout_session_service'
 
+# development? refers here to LOCAL development
 if Rails.env.development?
   Rails.configuration.stripe = {
     publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
@@ -7,8 +8,7 @@ if Rails.env.development?
     signing_secret:  ENV['STRIPE_WEBHOOK_SECRET_KEY_LOCAL']
   }
 else
-  p "PROUT env.dev? : #{Rails.env.development?}"
-  p ENV['STRIPE_WEBHOOK_SECRET_KEY_DEVELOPMENT']
+  # SECRET_KEY_DEVELOPMENT refers here to the Heroku-development app
   Rails.configuration.stripe = {
     publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
     secret_key:      ENV['STRIPE_SECRET_KEY'],
@@ -20,7 +20,6 @@ Stripe.api_key = Rails.configuration.stripe[:secret_key]
 StripeEvent.signing_secret = Rails.configuration.stripe[:signing_secret]
 
 StripeEvent.configure do |events|
-  # pp events
   events.subscribe 'checkout.session.completed' do |event|
     StripeCheckoutSessionService.new.call(event)
   end
