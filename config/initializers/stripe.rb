5b132ea44e914566bb70b1a85b4330bb5775aba5
Stephane Lafontaine <stephane@medqigong.ca>
2023-05-29 21:33:43 -0400

    Added message in config/initializers/stripe.rb for testing purposes

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -7,7 +7,8 @@ if Rails.env.development?
     signing_secret:  ENV['STRIPE_WEBHOOK_SECRET_KEY_LOCAL']
   }
 else
-  p "PROUT env NOT dev: #{Rails.env.development?}"
+  p "PROUT env.dev? : #{Rails.env.development?}"
+  p ENV['STRIPE_WEBHOOK_SECRET_KEY_DEVELOPMENT']
   Rails.configuration.stripe = {
     publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
     secret_key:      ENV['STRIPE_SECRET_KEY'],
@@ -19,7 +20,7 @@ Stripe.api_key = Rails.configuration.stripe[:secret_key]
 StripeEvent.signing_secret = Rails.configuration.stripe[:signing_secret]
 
 StripeEvent.configure do |events|
-  pp events
+  # pp events
   events.subscribe 'checkout.session.completed' do |event|
     StripeCheckoutSessionService.new.call(event)
   end
