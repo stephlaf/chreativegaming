00ffe55ddb223c9c83ee45d3da16ad8310dfa73f
Stephane Lafontaine <stephane@medqigong.ca>
2023-05-31 16:16:59 -0400

    Testing the response from API call

--- config/initializers/stripe_checkout_session_service.rb
+++ config/initializers/stripe_checkout_session_service.rb
@@ -1,7 +1,8 @@
 class StripeCheckoutSessionService
   def call(event)
     order = Order.find_by(checkout_session_id: event.data.object.id)
+    # p JSON.parse(event.data)
+    p event.data
     order.update(state: 'paid')
-    # p event.data
   end
 end
