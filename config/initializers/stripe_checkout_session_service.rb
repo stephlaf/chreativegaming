bb66747c851f868dcc20e74e7bad1548adc51ac0
Stephane Lafontaine <stephane@medqigong.ca>
2023-06-22 15:06:49 -0400

    Deleted unnecessary code in StripeCheckoutSessionService

--- config/initializers/stripe_checkout_session_service.rb
+++ config/initializers/stripe_checkout_session_service.rb
@@ -1,14 +1,4 @@
 class StripeCheckoutSessionService
   def call(event)
-    # order = Order.find_by(checkout_session_id: event.data.object.id)
-    # # p JSON.parse(event.data)
-    # p event
-    # p ""
-    # p event.data
-    # p ""
-    # p "EVENT TYPE IS:  #{event.type}"
-    # p ""
-    # # order.update(state: 'paid')
-
   end
 end
