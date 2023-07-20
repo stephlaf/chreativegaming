ebc0ccdea1ca1f850064d0e7d25fc8ee397b2345
Ines Alvergne <i.alvergne@gmail.com>
2023-07-20 14:28:13 -0400

    make sure that we load the images before checking for the width and height

--- app/javascript/controllers/blog_images_size_controller.js
+++ app/javascript/controllers/blog_images_size_controller.js
@@ -1,17 +1,20 @@
 import { Controller } from '@hotwired/stimulus'
+import imagesLoaded from 'imagesloaded'
 
 export default class extends Controller {
   static targets = ["images"]
 
   connect() {
-    this.imagesTargets.forEach(image => {
-      console.log(width, image.width);
-      console.log(height, image.height);
-      if (image.width > image.height) {
-        image.classList.add('landscape')
-      } else if (image.height > image.width)  {
-        image.classList.add('portrait')
-      }
-    })
+    imagesLoaded( this.element, () => {
+      this.imagesTargets.forEach(image => {
+        if (image.width > image.height) {
+          image.classList.add('landscape')
+          image.classList.remove('d-none')
+        } else if (image.height > image.width)  {
+          image.classList.add('portrait')
+          image.classList.remove('d-none')
+        }
+      })
+    });
   }
 }
