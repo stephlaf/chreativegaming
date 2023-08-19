766ddd6fc5fe13208ee98a7a6dfa01c73eaab6da
Stephane Lafontaine <stephane@medqigong.ca>
2023-08-19 15:42:41 -0400

    Added file with code to be run in the console in production to create orders from games already bought on Stripe

---
+++ db/order_creation_prod.rb
@@ -0,0 +1,39 @@
+# Shelby Masson, North Pole
+Order.create!(
+  state: 'paid',
+  amount_cents: 0,
+  charge_cents: 1000,
+  checkout_session_id: "cs_live_b1UkBsKzxaNhBOKKQxRJ1PF9uZYNNTWVU2It4U5bX57Jt91Fu8YPyjfxzE",
+  user_id: 161,
+  game_id: 6
+)
+
+# Forrestaz1, Last Survivor
+Order.create!(
+  state: 'paid',
+  amount_cents: 0,
+  charge_cents: 500,
+  checkout_session_id: "cs_live_b1BtuX9nPiVvG9bOw6D7kPH7PaSteDLIbo9IV61EewqYzDyfJI3k6F9pSU",
+  user_id: 160,
+  game_id: 5
+)
+
+# Bobo, Trapped
+Order.create!(
+  state: 'paid',
+  amount_cents: 0,
+  charge_cents: 500,
+  checkout_session_id: "cs_live_a19Jc5BLeucv9FtHsWKkka1Y7sy1uWW1Vpi2rnfy31qyA3AwmHq58nIxtr",
+  user_id: 90,
+  game_id: 4
+)
+
+# Bobo, Last Survivor
+Order.create!(
+  state: 'paid',
+  amount_cents: 0,
+  charge_cents: 500,
+  checkout_session_id: "cs_live_b1E1JCHlz5tsWXfw1aWU5akb5G0wHxzbznFLNa7gKVQSGz938YwPIp9nWy",
+  user_id: 90,
+  game_id: 5
+)
