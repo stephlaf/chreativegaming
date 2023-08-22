6e3bf722dcf6a1260bb1c336b9fe7729b23a7abf
Stephane Lafontaine <stephane@medqigong.ca>
2023-08-21 20:26:18 -0400

    Reverted to dev setup for Stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -11,12 +11,12 @@ else
   # SECRET_KEY_DEVELOPMENT refers here to the Heroku-development app
   # SECRET_KEY_PRODUCTION refers here to the Heroku-production app
   Rails.configuration.stripe = {
-    # publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
-    # secret_key:      ENV['STRIPE_SECRET_KEY'],
-    # signing_secret:  ENV['STRIPE_WEBHOOK_SECRET_KEY_DEVELOPMENT']
-    secret_key:      ENV['STRIPE_SECRET_KEY_PRODUCTION'],
-    publishable_key: ENV['STRIPE_PUBLISHABLE_KEY_PRODUCTION'],
-    signing_secret:  ENV['STRIPE_WEBHOOK_SECRET_KEY_PRODUCTION']
+    publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
+    secret_key:      ENV['STRIPE_SECRET_KEY'],
+    signing_secret:  ENV['STRIPE_WEBHOOK_SECRET_KEY_DEVELOPMENT']
+    # secret_key:      ENV['STRIPE_SECRET_KEY_PRODUCTION'],
+    # publishable_key: ENV['STRIPE_PUBLISHABLE_KEY_PRODUCTION'],
+    # signing_secret:  ENV['STRIPE_WEBHOOK_SECRET_KEY_PRODUCTION']
   }
 end
 
