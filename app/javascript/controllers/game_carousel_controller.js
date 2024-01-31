34ef73a592f23364398e5c34358e7e02f9668d4e
Ines Alvergne <i.alvergne@gmail.com>
2024-01-31 19:10:15 +0100

    basic carousel with images done

--- app/javascript/controllers/game_carousel_controller.js
+++ app/javascript/controllers/game_carousel_controller.js
@@ -1,9 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
 import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+
 
 export default class extends Controller {
   connect() {
     const swiper = new Swiper(".mySwiper", {
+      modules: [Navigation],
       navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
