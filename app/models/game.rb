7ebc1fea96ee934e07d583eee01349764cb656ae
Ines Alvergne <i.alvergne@gmail.com>
2024-02-13 18:06:23 +0100

    add validation for carousel visuals

--- app/models/game.rb
+++ app/models/game.rb
@@ -12,8 +12,8 @@ class Game < ApplicationRecord
   validates :download_link, presence: true
   validates :description, presence: true
   validates :description, length: { maximum: 180 }
-
   validates :thumbnail, presence: true
+  validate :carousel_visuals_count, if: -> { carousel_visuals.attached? }
 
   # Commented out temporarily until banner is actually in use
   # validates :banner, presence: true
@@ -29,5 +29,13 @@ class Game < ApplicationRecord
 
   def platforms_to_a
      self.available_platforms = self.available_platforms.nil? ? [] : JSON.parse(self.available_platforms)
+  end
+
+  private
+
+  def carousel_visuals_count
+    unless carousel_visuals.size == 4
+      errors.add(:carousel_visuals, "must have 4 visuals attached")
+    end
   end
 end
